Enabling XLA because at weird batch numbers getting wrong results

TensorFlow2/Segmentation/MaskRCNN/mask_rcnn/coco_metric.py
@@ -326,30 +326,26 @@ class MaskCOCO(COCO):
             is_image_mask=is_image_mask
         )
         
-        #encoded_masks = maskUtils.encode(segments)
-        encoded_masks = [
-            maskUtils.encode(instance_mask)
-            for instance_mask in segments
-        ]
+        encoded_masks = maskUtils.encode(segments)
 
       for box_index in range(int(detection_results['num_detections'][i])):
-        #if current_index % 1000 == 0:
-        #  logging.info('{}/{}'.format(current_index, num_detections))
+        if current_index % 1000 == 0:
+          logging.info('{}/{}'.format(current_index, num_detections))
 
         current_index += 1
-        if(detection_results['detection_scores'][i][box_index] >= threshold):
-          prediction = {
-              'image_id': int(image_id),
-              'bbox': detection_results['detection_boxes'][i][box_index],#.tolist(),
-              'score': detection_results['detection_scores'][i][box_index],
-              'category_id': int(
-                  detection_results['detection_classes'][i][box_index]),
-          }
+        #if(detection_results['detection_scores'][i][box_index] >= threshold):
+        prediction = {
+            'image_id': int(image_id),
+            'bbox': detection_results['detection_boxes'][i][box_index],#.tolist(),
+            'score': detection_results['detection_scores'][i][box_index],
+            'category_id': int(
+                detection_results['detection_classes'][i][box_index]),
+        }
 
-          if include_mask:
-            prediction['segmentation'] = encoded_masks[box_index]
+        if include_mask:
+          prediction['segmentation'] = encoded_masks[box_index]
 
-          predictions.append(prediction)
+        predictions.append(prediction)
     return predictions
 
 
@@ -410,8 +406,7 @@ def generate_segmentation_from_masks(masks,
   ref_boxes = expand_boxes(detected_boxes, scale)
   ref_boxes = ref_boxes.astype(np.int32)
   padded_mask = np.zeros((mask_height + 2, mask_width + 2), dtype=np.float32)
-  #segms = np.zeros((image_height, image_width, len(masks)), dtype=np.uint8, order='F')
-  segms = []
+  segms = np.zeros((image_height, image_width, len(masks)), dtype=np.uint8, order='F')
   for mask_ind, mask in enumerate(masks):
     im_mask = np.zeros((image_height, image_width), dtype=np.uint8, order='F')
     if is_image_mask:
@@ -437,10 +432,9 @@ def generate_segmentation_from_masks(masks,
 
       im_mask[y_0:y_1, x_0:x_1] = mask[(y_0 - ref_box[1]):(y_1 - ref_box[1]), (
           x_0 - ref_box[0]):(x_1 - ref_box[0])]
-    segms.append(im_mask)
+    segms[:,:,mask_ind] = im_mask
 
-  assert masks.shape[0] == len(segms)
-  
+  assert masks.shape[0] == segms.shape[2]
   return segms
 
 def parallel_encode(segments, out_q):
@@ -477,8 +471,8 @@ def load_predictions_parallel(index_range, include_mask, is_image_mask, detectio
       ]
 
     for box_index in range(int(detection_results['num_detections'][i])):
-      if current_index % 1000 == 0:
-        logging.info('{}/{}'.format(current_index, num_detections))
+      #if current_index % 1000 == 0:
+      #  logging.info('{}/{}'.format(current_index, num_detections))
 
       current_index += 1
 

TensorFlow2/Segmentation/MaskRCNN/mask_rcnn/tape_executor.py
@@ -20,7 +20,7 @@ import tensorflow as tf
 #mixed_precision.set_policy(policy)
 #Remeber to verify this runs on the OFFLOAD, Can run in Main file
 tf.config.optimizer.set_experimental_options({"auto_mixed_precision": True})
-#tf.config.optimizer.set_jit(True)
+tf.config.optimizer.set_jit(True)
 
 
 def train_and_eval(run_config, train_input_fn, eval_input_fn):

TensorFlow2/Segmentation/MaskRCNN/mask_rcnn/tf2/mask_rcnn_model.py
@@ -1172,20 +1172,12 @@ class TapeModel(object):
         #  tf.profiler.experimental.stop()
 
         
-        print(f"{MPI_rank(is_herring())}|Finished Joining Threads",flush=True)
-        
         end_total_infer = time.time()
         MPI.COMM_WORLD.barrier()
         if(not use_dist_coco_eval):
           
           predictions_list = evaluation.gather_result_from_all_processes(converted_predictions)
-          if MPI_rank(is_herring()) == 0:
-            print("Pred list is ", len(predictions_list))
-            total_preds = 0
-            for each in predictions_list:
-              total_preds += len(each)
-            print("Total preds is ", total_preds)
-
+          
           validation_json_file=self.params.val_json_file
           end_gather_result = time.time()
           #with cProfile.Profile() as pr:
@@ -1215,8 +1207,6 @@ class TapeModel(object):
           validation_json_file=self.params.val_json_file
           evaluation.fast_eval(converted_predictions, validation_json_file, use_ext, use_dist_coco_eval)
 
-        #ps = pstats.Stats(pr).sort_stats('cumtime')
-          #ps.print_stats()
         end_coco_eval = time.time()
         print(f"(avg, total) DataLoad ({data_load_total/steps}, {data_load_total}) predict ({predict_total/steps}, {predict_total})")
         print(f"Total Time {end_coco_eval-start_total_infer} Total Infer {end_total_infer - start_total_infer} gather res {end_gather_result - end_total_infer} coco_eval {end_coco_eval - end_gather_result}")