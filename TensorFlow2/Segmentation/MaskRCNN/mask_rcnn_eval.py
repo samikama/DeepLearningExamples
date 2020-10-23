4294fd6aa09aeedf6139683085a752526bea0eb5
Samuel Oshin <sboshin@amazon.com>
2020-10-23 01:33:09 +0000

    print tottal samples

--- TensorFlow2/Segmentation/MaskRCNN/mask_rcnn_eval.py
+++ TensorFlow2/Segmentation/MaskRCNN/mask_rcnn_eval.py
@@ -109,15 +109,17 @@ def do_eval(run_config, train_input_fn, eval_input_fn):
     args=[q, out_path, mrcnn_model]
     eval_workers=min(32, MPI_size())
     batches = []
+    total_samples = 0
     while 1:
       try:
         batches.append(next(mrcnn_model.eval_tdf)['features'])
-        print(batches[-1]['images'].shape)
+        total_samples += batches[-1]['images'].shape[0]
+
       except Exception as e:
         #Should only break when out of data
         break
     steps = len(batches)
-    print(steps)
+    print(total_samples)
     
     mrcnn_model.initialize_eval_model(batches[0])
    
